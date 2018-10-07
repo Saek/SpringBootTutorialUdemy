b21a51dd19bcabbd9fc092d2b689e05881230562
Kraisaek Thanapisitikul <saek_kus@hotmail.com>
2018-10-07 17:44:16 +0700

    add profiles

---
+++ profiles/src/main/java/com/example/configure/DataSource.java
@@ -0,0 +1,41 @@
+package com.example.configure;
+
+public class DataSource {
+	
+	private String server;
+    private int port;
+
+    public DataSource(){
+
+    }
+
+    public DataSource(String server, int port) {
+        this.server = server;
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    @Override
+    public String toString() {
+        return "DataSource{" +
+                "server='" + server + '\'' +
+                ", port=" + port +
+                '}';
+    }
+
+}
