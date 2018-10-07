f345be1ced90341395f5bfc5f0e9abb4dd008a20
Kraisaek Thanapisitikul <saek_kus@hotmail.com>
2018-10-07 22:52:52 +0700

    update with bower

---
+++ springboot-mvc/bower/src/main/java/com/toysforshots/BowerApplication.java
@@ -0,0 +1,12 @@
+package com.toysforshots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowerApplication.class, args);
+	}
+}
